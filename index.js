bb65d161b6bd31c61c79eb1ea26b9172ce0e6d1e
nrsabit <mailbox.sabit@gmail.com>
2023-05-20 20:12:07 +0600

    sort by asc and dsc api added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h7lvo9z.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,34 +25,57 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const toysCollection = client.db('toysDB').collection('toys')
+    const toysCollection = client.db("toysDB").collection("toys");
 
     // all toys api
-    app.get('/all-toys', async(req, res) => {
-      let limit = req.query.limit
-      if(limit !== 'all'){
-        limit = parseInt(limit)
+    app.get("/all-toys", async (req, res) => {
+      let limit = req.query.limit;
+      const sort = req.query.sort;
+      if (limit !== "all") {
+        limit = parseInt(limit);
       }
-      let result
-      if(limit !== 'all'){
-        result = await toysCollection.find().limit(limit).toArray()
-      }else{
-        result = await toysCollection.find().toArray()
+      let result;
+      if (limit !== "all") {
+        if (sort && sort === "asc") {
+          result = await toysCollection
+            .find()
+            .sort({ price: 1 })
+            .limit(limit)
+            .toArray();
+        } else if (sort && sort === "dsc") {
+          result = await toysCollection
+            .find()
+            .sort({ price: -1 })
+            .limit(limit)
+            .toArray();
+        } else {
+          result = await toysCollection.find().limit(limit).toArray();
+        }
+      } else {
+        if (sort && sort === "asc") {
+          result = await toysCollection.find().sort({ price: 1 }).toArray();
+        } else if (sort && sort === "dsc") {
+          result = await toysCollection.find().sort({ price: -1 }).toArray();
+        } else {
+          result = await toysCollection.find().toArray();
+        }
       }
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // toys by tab
-    app.get('/tab-toys', async(req, res) => {
-      const tab = req.query.tab 
-      const query = {category: tab}
-      const result = await toysCollection.find(query).limit(6).toArray()
-      res.send(result)
-    })
+    app.get("/tab-toys", async (req, res) => {
+      const tab = req.query.tab;
+      const query = { category: tab };
+      const result = await toysCollection.find(query).limit(6).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -61,11 +83,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Motor Toys Server is Running')
-})
+app.get("/", (req, res) => {
+  res.send("Motor Toys Server is Running");
+});
 
 app.listen(port, () => {
-    console.log(`Motor Toys server is running on port: ${port}`)
-})
+  console.log(`Motor Toys server is running on port: ${port}`);
+});

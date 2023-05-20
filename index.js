e5748a1678f07957dc42589bb93acc22f837cc92
nrsabit <mailbox.sabit@gmail.com>
2023-05-21 02:35:01 +0600

    single toy api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -70,6 +70,14 @@ async function run() {
       const result = await toysCollection.find(query).limit(6).toArray();
       res.send(result);
     });
+
+    // single toy
+    app.get('/toys/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)}
+      const result = await toysCollection.findOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

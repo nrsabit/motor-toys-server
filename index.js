0eea10d4f8c23eedacd0792765c57e4ed6c29f82
nrsabit <mailbox.sabit@gmail.com>
2023-05-21 06:16:14 +0600

    api setup for delete

--- index.js
+++ index.js
@@ -95,6 +95,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete a toy 
+    app.delete('/delete-toy/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await toysCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
